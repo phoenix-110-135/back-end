c0fb127dffc45dc7a9bacdaae1d1024d06324cf9
phoenix-110-135 <a87h97j2022@gmail.com>
2025-03-25 17:25:44 +0330

    udate

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ def blog_view(requests):
     return render(requests,'blog/blog-home.html')
 
 def blog_single(requests):
-    return render(requests,'blog/blog-single.html')
+    context= {"title":"title_1","content":"lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image","author":"ALI MOHAMMDI NIA"}
+    return render(requests,'blog/blog-single.html',context)
