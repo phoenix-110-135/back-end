a5c5c6beaed1003d9df0f24d4a2c0f57340d9684
phoenix-110-135 <a87h97j2022@gmail.com>
2025-04-10 12:57:35 +0330

    update my database

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    # image
+    # author 
+
     title = models.CharField(max_length=255)
     contex = models.TextField()
+    # tag
+    # category
+    counted_view = models.IntegerField(default=0) 
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
     
