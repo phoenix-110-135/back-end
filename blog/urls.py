160da096bd60933e5ff0826e074621cb91d0f2a1
phoenix-110-135 <a87h97j2022@gmail.com>
2025-04-26 18:57:52 +0330

    connect my database with html

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,12 @@
 from blog.views import *
 from django.urls import path
 
-
 app_name = 'blog'
 
 urlpatterns = [
     path("",blog_view,name="blog"),
     path("single",blog_single,name="single"),
-
+    path("test",test,name="test"),
     
     
     
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from blog.models import Post 
 # Create your views here.
 def blog_view(requests):
     return render(requests,'blog/blog-home.html')
@@ -7,3 +7,8 @@ def blog_view(requests):
 def blog_single(requests):
     context= {"title":"title_1","content":"lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image","author":"ALI MOHAMMDI NIA"}
     return render(requests,'blog/blog-single.html',context)
+
+def test(requests):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(requests,'test.html',context)
