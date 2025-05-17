6f3be9728c6a6363e8574463d51ebd076cd4ea50
phoenix-110-135 <a87h97j2022@gmail.com>
2025-05-17 16:00:15 +0330

    update

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,9 @@ def blog_view(requests):
     return render(requests,'blog/blog-home.html',context)
 
 def blog_single(requests,pid):
-    post = get_object_or_404(Post,pk=pid)
+    post = get_object_or_404(Post,pk=pid,status=1)
+    # posts = Post.objects.filter(status=1)
+    # post get_object_or_404(Post,pk=pid,status=1)
     context = {'posts' : post}
     # context= {"title":"title_1","content":"lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image","author":"ALI MOHAMMDI NIA"}
     return render(requests,'blog/blog-single.html',context)
