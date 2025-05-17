97a4ddd5f49178743da101e9ce877ef6a65e8560
phoenix-110-135 <a87h97j2022@gmail.com>
2025-05-17 15:47:27 +0330

    update

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path("",blog_view,name="blog"),
-    path("single",blog_single,name="single"),
-    path("test",test,name="test"),
+    path("<int:pid>",blog_single,name="single"),
+    # path("post-<int:pid>",test,name="test"),
     
     
     
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from blog.models import Post 
 # Create your views here.
 def blog_view(requests):
-    return render(requests,'blog/blog-home.html')
+    post = Post.objects.filter(status=1)
+    context = {'posts':post}
+    return render(requests,'blog/blog-home.html',context)
 
-def blog_single(requests):
-    context= {"title":"title_1","content":"lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image","author":"ALI MOHAMMDI NIA"}
+def blog_single(requests,pid):
+    post = get_object_or_404(Post,pk=pid)
+    context = {'posts' : post}
+    # context= {"title":"title_1","content":"lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image lorem ipsum a difalt image","author":"ALI MOHAMMDI NIA"}
     return render(requests,'blog/blog-single.html',context)
 
-def test(requests):
-    posts = Post.objects.all()
-    context = {'posts':posts}
+def test(requests,pid):
+    # posts = Post.objects.get(id=pid)
+    post = get_object_or_404(Post,pk=pid)
+    context = {'posts' : post}
+    # context = {'name' : name}
     return render(requests,'test.html',context)
