ccc21279aa7487dfdee654b8269ec2bba217f798
phoenix-110-135 <a87h97j2022@gmail.com>
2026-05-14 02:10:49 +0330

    add postscategories inclusiontags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,31 +5,30 @@ from blog.models import Category
 register = template.Library()
 
 
-@register.simple_tag(name="totalpost")
-def func():
+@register.simple_tag(name='totalposts')
+def function():
     posts = Post.objects.filter(status=1).count()
-    return posts 
+    return posts
 
-@register.simple_tag(name="posts")
-def func():
+@register.simple_tag(name='posts')
+def function():
     posts = Post.objects.filter(status=1)
-    return posts 
-
+    return posts
 
 @register.filter
-def snippet(value,args=20):
-    return value[:args] + "...."
+def snippet(value, arg=20):
+    return value[:arg] + "..."
 
-@register.inclusion_tag("blog/blog-popular-post.html")
-def latestsposts(arg=3):
-    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
-    return {"posts": posts}
+@register.inclusion_tag('blog/blog-popular-post.html')
+def latestposts(args=2):
+    posts = Post.objects.filter(status=1).order_by('published_date').reverse()[:args]
+    return {'posts': posts}
 
-@register.inclusion_tag("blog/blog-category.html")
-def postcategory():
-    posts = Post.objects.filter(status=1).order_by('published_date')
+@register.inclusion_tag('blog/blog-category.html')
+def postscategories():
     cat_dict = {}
+    posts = Post.objects.filter(status=1)
     categories = Category.objects.all()
-    for name in categories: 
-        cat_dict[name]=posts.filter(categories=name).count()
-    return {'categories' : cat_dict}
+    for name in categories :
+        cat_dict[name] = posts.filter(category=name).count()
+    return {"categories":cat_dict}
